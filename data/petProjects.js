440aeb56e643de8e95e689ce3aa82edf47243b4d
mhrkshmk <makoueimehrkish@gmail.com>
2026-03-15 21:24:40 -0700

    The second edition of the static part of portfolio website

--- data/petProjects.js
+++ data/petProjects.js
@@ -6,15 +6,11 @@ export default [
         category: "game"
     },
     {
-        name: "Tenzies: Assembly",
-        description: "A game where you play Tenzies while programming language labels are removed one by one.",
-        tags: ["JavaScript", "TypeScript", "React", "CSS3"],
-        category: "game"
-    },
-    {
-        name: "Tenzies: Assembly",
-        description: "A game where you play Tenzies while programming language labels are removed one by one.",
-        tags: ["JavaScript", "TypeScript", "React", "CSS3"],
-        category: "game"
+        name: "Quizzical App",
+        description: "An interactive trivia quiz app where users choose quiz settings, answer questions, and get a final score.",
+        tags: ["JavaScript", "TypeScript", "HTML", "React", "CSS3"],
+        category: "Game",
+        githubLink: "https://github.com/mhrkshmk/Quizzical",
+        previewLink: "https://mhrkshmk.github.io"
     },
 ]
--- scripts/renderPetProjects.js
+++ scripts/renderPetProjects.js
@@ -13,8 +13,8 @@ export default function renderPetProjects() {
             <header class="flex items-center justify-between">
                 <h3 class="text-[9px] font-semibold text-[#50C878] px-2 py-1 border border-[#50C878] rounded-full">${project.category}</h3>
                 <div class="flex gap-2 items-center">
-                    <img src="./assets/icons/github2.svg" class="h-7 w-7 rounded-full" alt="GitHub link" />
-                    <a class="cursor-pointer" href="#"><img src="./assets/icons/right-arrow.svg" class="h-6 w-6 rounded-full border border-[#DBDBDB] p-1 group-hover/project-card:-rotate-45 transition-all duration-[600ms]" alt="a right arrow" /></a>
+                    <a class="h-fit w-fit" target="_blank" rel="noopener noreferrer" href="https://github.com/mhrkshmk/Quizzical.git"><img src="./assets/icons/github2.svg" class="h-7 w-7 rounded-full" alt="GitHub link" /></a>
+                    <a class="cursor-pointer" target="_blank" rel="noopener noreferrer" href="#"><img src="./assets/icons/right-arrow.svg" class="h-6 w-6 rounded-full border border-[#DBDBDB] p-1 group-hover/project-card:-rotate-45 transition-all duration-[600ms]" alt="a right arrow" /></a>
                 </div>
             </header>
             <div class="py-[1.25rem]">
